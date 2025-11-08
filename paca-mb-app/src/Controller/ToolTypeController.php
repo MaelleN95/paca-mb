071a2eba70be8054db265e50b89b3c899830a8ff
Maëlle Nioche <maelle.nioche@gmail.com>
2025-11-08 22:53:01 +0100

    feat(toolType): add tooltype_index

--- paca-mb-app/src/Controller/ToolTypeController.php
+++ paca-mb-app/src/Controller/ToolTypeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\ToolType;
+use App\Repository\ToolTypeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,6 +11,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/tool/type', name: 'tooltype_')]
 final class ToolTypeController extends AbstractController
 {
+    #[Route('/', name: 'index')]
+    public function index(ToolTypeRepository $toolTypeRepository): Response
+    {
+        $toolsTypes = $toolTypeRepository->findBy([], ['name' => 'ASC']);
+
+        return $this->render('tool/index.html.twig', [
+            'tools' => $toolsTypes,
+        ]);
+    }
+
     #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'])]
     public function show(ToolType $toolType): Response
     {
